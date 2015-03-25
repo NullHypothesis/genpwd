195765a6cddff336719f1c5e2ddcd68705b6439b
Philipp Winter <phw@nymity.ch>
2015-03-25 16:23:38 +0100

    Make code PEP 8-compliant.

--- genpwd.py
+++ genpwd.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (C) 2008, 2011, 2012, 2013, 2014 Philipp Winter <phw@nymity.ch>
+# Copyright (C) 2008, 2011-2015 Philipp Winter <phw@nymity.ch>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,17 +19,18 @@ import os
 import math
 import argparse
 
+
 def parse_args():
     """Parse the given command line arguments."""
 
     parser = argparse.ArgumentParser(description="Generate a random password.")
-    parser.add_argument("-l",
-                        "--length",
+    parser.add_argument("-l", "--length",
                         type=int,
                         default=30,
                         help="Password length in ASCII characters.")
 
     return parser.parse_args()
+
 
 def get_rand_char(alphabet):
     """Get a random character in the given character alphabet."""
@@ -39,6 +40,7 @@ def get_rand_char(alphabet):
         char = os.urandom(1)
 
     return char
+
 
 def generate(args):
     """Generate and print a random password."""
@@ -51,6 +53,7 @@ def generate(args):
     entropy = math.log(len(alphabet) ** args.length, 2)
 
     print "%d-bit password:  %s" % (entropy, password)
+
 
 if __name__ == "__main__":
     generate(parse_args())
